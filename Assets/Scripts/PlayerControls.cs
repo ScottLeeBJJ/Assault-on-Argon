1e7b6b13867856e8faddce6c072ca4b89b3e8aa4
ScottLeeBJJ <scottlee5455@outlook.com>
2022-05-13 04:10:38 -0700

    clamped player movement
    this prevents players from exiting the camera on the rail so the player ship is always in view.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] float throwSpeed = 20f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 10f;
 
     void Update()
     {
+
+      // the positioning of these variables is critical, will not work unless laid out like this
 
       float xThrow = Input.GetAxis("Horizontal");     
       float yThrow = Input.GetAxis("Vertical");
 
       float xOffset = xThrow * Time.deltaTime * throwSpeed;
-      float newXPos = transform.localPosition.x + xOffset;
-
       float yOffset = yThrow * Time.deltaTime * throwSpeed;
-      float newYPos = transform.localPosition.y + yOffset;
 
-      transform.localPosition = new Vector3 
-      (newXPos, newYPos, transform.localPosition.z);
+      float rawXPos = transform.localPosition.x + xOffset;
+      float rawYPos = transform.localPosition.y + yOffset;
+
+      float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+      float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+
+      transform.localPosition = new Vector3 (clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
