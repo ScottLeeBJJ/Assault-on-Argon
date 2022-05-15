6b0d5249c35d6725506c5ebd21196bccabbf7473
ScottLeeBJJ <scottlee5455@outlook.com>
2022-05-14 23:03:08 -0700

    pitch, roll, and yaw
    created the pitch, roll and yaw of the ship as the player moves it around the screen

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,6 +8,16 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] float xRange = 10f;
     [SerializeField] float yRange = 7f;
 
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float positionPitchFactor = -2f;
+
+    [SerializeField] float positionYawFactor = 2f;
+
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow;
+    float yThrow;
+
     void Update()
     {
       ProcessTranslation();
@@ -16,16 +26,25 @@ public class PlayerControls : MonoBehaviour
 
     void ProcessRotation()
     {
+      float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+      float pitchDueToControlThrow = yThrow * controlPitchFactor;
+
+      float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+
+      float pitch = pitchDueToPosition + pitchDueToControlThrow;
+      float yaw = yawDueToPosition;
+      float roll = xThrow * controlRollFactor;
+
       // A quaternion represents two things. It has an x, y, and z component, which represents the axis about which a rotation will occur. It also has a w component, which represents the amount of rotation which will occur about this axis. In short, a vector, and a float.
-      transform.localRotation = Quaternion.Euler(-30f, 30f, 0f); 
+      transform.localRotation = Quaternion.Euler(pitch, yaw, roll); 
     }
 
     void ProcessTranslation()
     {
       // the positioning of these variables is critical, will not work unless laid out like this
 
-      float xThrow = Input.GetAxis("Horizontal");     
-      float yThrow = Input.GetAxis("Vertical");
+      xThrow = Input.GetAxis("Horizontal");     
+      yThrow = Input.GetAxis("Vertical");
 
       float xOffset = xThrow * Time.deltaTime * throwSpeed;
       float yOffset = yThrow * Time.deltaTime * throwSpeed;
