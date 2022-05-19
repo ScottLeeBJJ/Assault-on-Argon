db6f981c3b81fd89882a76c4d9b97380a1cb1053
ScottLeeBJJ <scottlee5455@outlook.com>
2022-05-19 01:16:37 -0700

    started building scoreboard system

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,8 +7,18 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
 
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+
+    void Start() 
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
     void OnParticleCollision(GameObject other) 
     {
+        scoreBoard.IncreaseScore(scorePerHit);
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
