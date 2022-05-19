b7dc47457a25dbe39bf72f7ab23110e3387838ec
ScottLeeBJJ <scottlee5455@outlook.com>
2022-05-19 02:15:00 -0700

    hit marks for enemies that are shot

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
 
     [SerializeField] int scorePerHit = 15;
+
+    [SerializeField] int hitPoints = 10;
 
     ScoreBoard scoreBoard;
 
@@ -19,11 +22,18 @@ public class Enemy : MonoBehaviour
     void OnParticleCollision(GameObject other) 
     {
         ProcessHit();
+        if (hitPoints < 1)
+        {
         KillEnemy();
+        }
     }
 
     void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        hitPoints--;
+
         scoreBoard.IncreaseScore(scorePerHit);
     }
 
